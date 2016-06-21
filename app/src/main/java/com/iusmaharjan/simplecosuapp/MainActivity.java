2fbcea20fecaa6c181062b433c41e6d074a172e4
Ayush Maharjan <ius.maharjan@gmail.com>
2016-06-21 14:50:07 +0545

    simple pinning and unpinning done

--- app/src/main/java/com/iusmaharjan/simplecosuapp/MainActivity.java
+++ app/src/main/java/com/iusmaharjan/simplecosuapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.iusmaharjan.simplecosuapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    /**
+     * Pins the app when the lock button is pressed
+     */
+    @OnClick(R.id.lock_button)
+    public void lock() {
+        Timber.d("Pinning the app");
+        this.startLockTask();
+    }
+
+    /**
+     * Unpins the app when the lock button is pressed
+     */
+    @OnClick(R.id.unlock_button)
+    public void unlock() {
+        Timber.d("Unpinning the app");
+        this.stopLockTask();
     }
 }
